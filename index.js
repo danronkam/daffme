d5322f97df79c22369317377f0783ae7c4964ba2
danronkam <danronkam@gmail.com>
2024-01-31 12:25:22 -0800

    database connected

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoConn = process.env.ATLAS_URI;
+
+mongoose.connect(mongoConn);
+
+const db = mongoose.connection;
+
+db.on('error', (e) => {
+    console.log(e)
+});
+
+db.once('connected', () => {
+    console.log("DB C")
+})
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(2120, () => {
+    console.log('Hamster Running on Wheel: 2120')
+})
