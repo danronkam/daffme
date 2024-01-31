23cd9c1b85fa25230d8b2f39df204a767e1cd6db
danronkam <danronkam@gmail.com>
2024-01-31 08:57:20 -0800

    going live

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+
+const app = express ();
+app.use(express.json());
+
+const PORT = process.env.PORT || 2120;
+
+
+app.listen(PORT, () => {
+    console.log("Hamster Running on PORT:", PORT);
+});
+
+app.get("/staus", (req, res) => {
+    const status = {
+        "Status": "(🖒^^)"
+    };
+
+    res.send(status);
+});
