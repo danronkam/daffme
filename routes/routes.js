b8d7b727b7266f6db3650740ebd9af5c6107ec61
danronkam <danronkam@gmail.com>
2024-02-02 21:52:54 -0800

    routes

--- routes/routes.js
+++ routes/routes.js
@@ -5,12 +5,26 @@ const router = express.Router();
 module.exports = router;
 
 //Post Method
-
+router.post('/post', (req, res) => {
+    res.send("Post API")
+})
 
 //Get All Method
+router.get('/getAll', (req, res) =>{
+    res.send("Get All API")
+})
 
 // GEt by ID Method
+router.get('/getOne/:id', (req, res) => {
+    res.send("Get by ID API")
+})
 
 //Update by ID Method
+router.patch('update/:id', (req, res) => {
+    res.send("Update by ID API")
+})
 
 //Delete by ID Method
+router.delete('/delete/:id', (req, res) => {
+    res.send('Delete by ID APi')
+})
